1837fe535e5ada8f1b8e39e7ef5bdbf88eee27ee
Oluwafikayo Ajayi <oluwafikayoajayi@gmail.com>
2021-03-22 12:32:29 +0100

    Movie list implementation

--- src/containers/List.js
+++ src/containers/List.js
@@ -1,9 +1,41 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Card from '../components/Card';
 
-class List extends Component{
-    render(){
-        return <Card />;
+class List extends Component {
+    constructor() {
+        super();
+        this.state = {
+            data: [],
+            loading: true,
+        };
+    }
+
+    async componentDidMount() {
+
+        try {
+
+            const movies = await fetch('../../src/assets/data.json');
+            const moviesJSON = await movies.json();
+
+            if (moviesJSON) {
+                this.setState({ data: moviesJSON, loading: false, });
+            }
+
+        } catch (error) {
+            this.setState({ data: [], loading: false });
+        }
+
+    }
+
+
+    render() {
+        const { data, loading } = this.state;
+        if (loading) {
+            return <div>Loading...</div>;
+        }
+
+
+        return data.map((movie) => <Card key={movie.id} movie={movie} />);
     }
 };
 
